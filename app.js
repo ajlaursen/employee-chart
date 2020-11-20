906deaea3501d6d2b2b79dc74082ae926ead3f5c
Alex Laursen <ajlaursen@aol.com>
2020-11-20 01:11:00 -0500

    finished JS app

--- app.js
+++ app.js
@@ -9,11 +9,124 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const Employee = require( "./lib/Employee" );
+const { log } = require( "console" );
 
+let employeeArray = [];
+
+
+// questions that will be asked of all employees
+const employeeQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name?",
+    default: "No Name Given",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is your ID number?",
+    default: "No ID Given",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your E-mail address?",
+    default: "No Email Given",
+  },
+];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+
+// function that prompts user for all questions other than new questions
+const promptUser = (array) => {
+  return inquirer.prompt(array).then((answers) => {
+    return answers
+  }).catch(error => console.error(error));
+};
+
+// functions for each employee based on what user selected. 
+// Adds neccesary question calls above function. 
+// fills constructor with information to push to array
+
+const newManager = () => {
+  employeeQuestions.push({
+    type: "input",
+    name: "officeNumber",
+    message: "What is your office number?",
+    default: "No Office Number Given",
+  });
+ 
+  promptUser(employeeQuestions).then((answers) => {
+  console.log("answers", answers);
+  const employee = new Manager(answers.name, answers.id, answers.email, answers.officeNumber, "Manager")
+  employeeArray.push(employee)
+  newEmployee()})
+};
+
+const newEngineer = () => {
+  employeeQuestions.push({
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?",
+    default: "No Username Given",
+  });
+  promptUser(employeeQuestions).then((answers) => {
+  const employee = new Engineer(answers.name, answers.id, answers.email, answers.github, "Engineer")
+  employeeArray.push(employee)
+  newEmployee()})
+};
+
+const newIntern = () => {
+  employeeQuestions.push({
+    type: "input",
+    name: "school",
+    message: "What school do you attend?",
+    default: "No School Given",
+  });
+  promptUser(employeeQuestions).then((answers) => {
+  const employee = new Intern(answers.name, answers.id, answers.email, answers.school, "Intern")
+  employeeArray.push(employee)
+  newEmployee()})
+};
+
+const newEmployee = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "newEmployee",
+        message: "Would you like to add another employee",
+        choices: ["No", "Manager", "Engineer", "Intern"],
+      },
+    ])
+    .then((answers) => {
+      switch (answers.newEmployee) {
+        case "Manager":
+          newManager();
+          break;
+        case "Engineer":
+          newEngineer();
+          break;
+        case "Intern":
+          newIntern();
+          break;
+        default:
+          renderTeam(employeeArray);
+      }
+    }).catch(error => console.error(error));
+};
+
+
+const renderTeam = (array) => {
+    fs.writeFile(outputPath, render(array), function(){})
+}
+
+
+newEmployee();
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
